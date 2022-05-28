e3df3bb2799ed540d1c6f59a57aaf1648089b284
Natia Kerdzaia <57873603+natiaker@users.noreply.github.com>
2022-05-28 22:15:46 +0400

    feat:  add date, form validation and summary

--- pages/form/form.js
+++ pages/form/form.js
@@ -3,7 +3,7 @@ function nameValidation() {
     let format = /^[A-Za-z]+$/g;
     if(fname != '') {    
       if( !fname.match(format)){
-        document.getElementById("fname-error").innerHTML = "Name must not contain any numbers";
+        document.getElementById("fname-error").innerHTML = "Name must contain only letters";
         document.getElementById("fname").style.border = "2px solid #fd9207";
         return false;
       }else if (fname.length < 4){
@@ -15,6 +15,9 @@ function nameValidation() {
         document.getElementById("fname").style.border = "none";
         return true;
       }
+    } else {
+      document.getElementById("fname-error").innerHTML = "You can't leave this field empty";
+      document.getElementById("fname").style.border = "2px solid #fd9207";
     }
 }
 
@@ -23,7 +26,7 @@ function surnameValidation() {
     let format = /^[A-Za-z]+$/g;
     if(surname != '') {    
       if( !surname.match(format)){
-        document.getElementById("surname-error").innerHTML = "Surname must not contain any numbers";
+        document.getElementById("surname-error").innerHTML = "Surname must contain only letters";
         document.getElementById("surname").style.border = "2px solid #fd9207";
         return false;
       }else if (surname.length < 5){
@@ -35,12 +38,25 @@ function surnameValidation() {
         document.getElementById("surname").style.border = "none";
         return true;
       }
+    } else {
+      document.getElementById("surname-error").innerHTML = "You can't leave this field empty";
+      document.getElementById("surname").style.border = "2px solid #fd9207";
     }
 }
 
+//date validation
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0') ;
+var yyyy = today.getFullYear();
+
+today = yyyy + '-' + mm + '-' +(+dd + 1);
+document.getElementById("date").setAttribute('min',today);
+
+
 function streetValidation() {
     let street = document.getElementById("street").value;
-    let format = /^[0-9a-zA-Z]+$/;
+    let format = /^[0-9a-zA-Z.-\s]+$/;
     if(street != '') {    
       if( !street.match(format)){
         document.getElementById("street-error").innerHTML = "You can't use such symbols";
@@ -55,6 +71,9 @@ function streetValidation() {
         document.getElementById("street").style.border = "none";
         return true;
       }
+    } else {
+      document.getElementById("street-error").innerHTML = "You can't leave this field empty";
+      document.getElementById("street").style.border = "2px solid #fd9207";
     }
 }
 
@@ -63,7 +82,7 @@ function houseValidation() {
     let format = /^[1-9]+[0-9]*$/;
     if(house != '') {    
         if( !house.match(format)){
-          document.getElementById("house-error").innerHTML = "You can't use negative numbers";
+          document.getElementById("house-error").innerHTML = "You can use only positive numbers";
           document.getElementById("house").style.border = "2px solid #fd9207";
           return false;
         }else{
@@ -71,12 +90,15 @@ function houseValidation() {
           document.getElementById("house").style.border = "none";
           return true;
         }
+    } else {
+      document.getElementById("house-error").innerHTML = "You can't leave this field empty";
+      document.getElementById("house").style.border = "2px solid #fd9207";
     }
 }
 
 function flatValidation() {
     let flat = document.getElementById("flat").value;
-    let format = /^[1-9–]+[-0-9–]*$/;
+    let format = /^[1-9]+[-0-9–]*$/;
     if(flat != '') {    
         if( !flat.match(format)){
           document.getElementById("flat-error").innerHTML = "You can't use such symbols";
@@ -87,14 +109,17 @@ function flatValidation() {
           document.getElementById("flat").style.border = "none";
           return true;
         }
+    } else {
+      document.getElementById("flat-error").innerHTML = "You can't leave this field empty";
+      document.getElementById("flat").style.border = "2px solid #fd9207";
     }
 }
 
+//restrict checkbox
 var checks = document.querySelectorAll(".checked");
 var max = 2;
 for (var i = 0; i < checks.length; i++) 
   checks[i].onclick = selectiveCheck;
-
 
 function selectiveCheck (event) {
   var checkedChecks = document.querySelectorAll(".checked:checked");
@@ -103,4 +128,61 @@ function selectiveCheck (event) {
     return false;
   }
   document.getElementById("checkbox-error").innerHTML = "";
+}
+
+//check if any input field is empty. disable/enable submit button
+document.getElementById('submit').disabled = true;
+
+function validateForm() {
+  let a = document.forms["myForm"]["fname"].value;
+  let b = document.forms["myForm"]["surname"].value;
+  let c = document.forms["myForm"]["date"].value;
+  let d = document.forms["myForm"]["street"].value;
+  let e = document.forms["myForm"]["house"].value;
+  let f = document.forms["myForm"]["flat"].value;
+  let g = document.forms["myForm"]["payment"].value;
+
+  let list = [a,b,c,d,e,f,g]; 
+  if(list.every(elem => elem != "")) {
+    document.getElementById('submit').disabled = false;
+    document.getElementById('submit').classList = "submit-style";
+  }
+}
+
+// summarize information
+function summarizeInfo(event) {
+  event.preventDefault();
+
+  let fname = document.getElementById("fname").value;
+  let surname = document.getElementById("surname").value;
+  let date = document.getElementById("date").value;
+  let street = document.getElementById("street").value;
+  let house = document.getElementById("house").value;
+  let flat = document.getElementById("flat").value;
+  let payment = document.querySelector("input[type='radio']:checked").value;
+  let gifts = document.querySelectorAll("input[type='checkbox']:checked");
+  let summaryInfo = document.getElementById("summaryInfo");
+  let backToMain = document.getElementById("backToMain");
+
+  let info = ` <h2 style="color:#fd9207; text-align:center;"> Congrats! Your Order has been confirmed </h2>
+  Hi ${fname} ${surname}, <br><br> We will deliver your order at Str.${street}, house N${house}, flat N${flat} on ${date}. <br> <br>
+  Your Payment method is: ${payment} <br> <br>`;
+
+  if (gifts.length == 2) {
+      info += `You choosed for gifts: ${gifts[0].value} and ${gifts[1].value} <br><br>
+      Thank you ${fname}, visit us again! <br> <br>
+      Enjoy the reading!!`;
+  } else if (gifts.length == 1) {
+      info += `You choosed for gift: ${gifts[0].value}! <br><br>
+      Thank you ${fname}, visit us again! <br> <br>
+      Enjoy the reading!!`;
+  } else {
+      info += `Thank you ${fname}, visit us again! <br><br> Enjoy the reading!!`;
+  }
+
+  document.getElementById("myForm").style.display = "none";
+  summaryInfo.innerHTML = info;
+  summaryInfo.classList = "summaryInfo-style"; 
+  backToMain.innerHTML = `<a href="../main/index.html">Main Page</a>`;
+  backToMain.classList = "backToMain-style"; 
 }
